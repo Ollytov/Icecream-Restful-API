37176094427f8d407c0e3e3cc3c7cfe9b1d635d3
Daniel Maloof <daniel.maloof@gmail.com>
2016-02-05 10:18:45 -0500

    Add comment to generic exception

--- src/main/java/org/danielmaloof/icecream/exception/GenericExceptionMapper.java
+++ src/main/java/org/danielmaloof/icecream/exception/GenericExceptionMapper.java
@@ -8,7 +8,11 @@ import org.danielmaloof.icecream.model.ErrorMessage;
 
 @Provider
 public class GenericExceptionMapper implements ExceptionMapper<Throwable>{
-
+	/*
+	 * This is a generic catch all, so that any errors not caught will return a JSON object rather than
+	 * an HTML document. This isn't really the best practice, but I wanted to get it down here while I 
+	 * was looking for the best exceptions to catch.
+	 */
 	@Override
 	public Response toResponse(Throwable ex) {
 		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), 500, "linktowebsite.com");
